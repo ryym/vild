55425b57bb958086e7026979e833a3996daf2ecc
ryym <ryym.64@gmail.com>
2020-10-03 20:44:03 +0900

    feat: Export extractErrorMessages

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './builtin';
 export * from './checker';
 export * from './messages';
 export * from './meta';
+export * from './util/extractErrorMessages';
 export * from './testObject';
 export * from './validator';
 export * from './vild';
